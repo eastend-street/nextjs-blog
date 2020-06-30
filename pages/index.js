b5de751c620253ce840d1d5ca2388533365b0ef8
eastend-street <suminari.yamada@gmail.com>
2020-06-29 23:01:51 -0700

    added getStaticProps to get blog meta data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+// 静的生成の場合に使う(SSRではないので、ビルド時にのみ実行される。)
+// ユーザーごとに内容が変わるページなどには使えない。
+// SSRではgetServerSidePropsを使う。
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +28,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
